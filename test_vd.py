febbb1be08908e3992e98491884b6cd9280f5b90
ldphu1 <ducphu0216@gmail.com>
2026-03-22 22:18:32 +0700

    Add files via upload

--- test_vd.py
+++ test_vd.py
@@ -1,54 +1,53 @@
-import os.path
 import cv2
-import glob
-from collections import defaultdict
 import numpy as np
 from ultralytics import YOLO
+import torch
+from collections import defaultdict
 
-def process_image_sequence(model_path, sequence_folder, output_path):
+
+def process_video(model_path, video_path, output_path):
     model = YOLO(model_path)
+    cap = cv2.VideoCapture(video_path)
 
-    img_files = glob.glob(os.path.join(sequence_folder, "*.jpg"))
+    if not cap.isOpened():
+        print(f"Lỗi: Không thể mở video tại đường dẫn '{video_path}'")
+        return
 
-    track_his = defaultdict(lambda : [])
+    track_his = defaultdict(lambda:[])
 
     previous_y_positions = {}
     crossed_count = 0
     counted_ids = set()
 
-    if not img_files:
-        print("Not found")
-    else:
-        frist_frame = cv2.imread(img_files[0])
-        height, width, _ = frist_frame.shape
+    width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+    height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
 
-        LINE_Y = height - 100
+    out = cv2.VideoWriter(output_path, fourcc, 24.0, (int(width), int(height)))
 
-        output_path = output_path
-        fourcc = cv2.VideoWriter_fourcc(*"mp4v")
-        out = cv2.VideoWriter(output_path, fourcc, 24.0, (width, height))
-
-        for img_file in img_files:
-            frame = cv2.imread(img_file)
-
-            result = model.track(frame, persist=True, verbose=False )
+    while cap.isOpened():
+        flag, frame = cap.read()
+        if not flag:
+            break
+        result = model.track(frame, persist=True, verbose=False, conf=0.6 )
+        for r in result:
             annotated_frame = frame.copy()
 
-            if result[0].boxes.id is not None:
-                boxes_xyxy = result[0].boxes.xyxy.cpu()
-                boxes_xywh = result[0].boxes.xywh.cpu()
-                scores = result[0].boxes.conf.cpu()
-                classes = result[0].boxes.cls.cpu()
+            if r.boxes.id is not None:
+                boxes_xyxy = r.boxes.xyxy.cpu()
+                boxes_xywh = r.boxes.xywh.cpu()
+                scores = r.boxes.conf.cpu()
+                classes = r.boxes.cls.cpu()
 
                 for box_xy, score, cls in zip(boxes_xyxy, scores, classes):
-                    if score < 0.5:
+                    if score < 0.6:
                         continue
                     x1, y1, x2, y2 = map(int, box_xy)
                     label = f"{model.names[int(cls)]} {score:.2f}"
                     cv2.rectangle(annotated_frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
-                    cv2.putText(annotated_frame, label, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2)
+                    cv2.putText(annotated_frame, label, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 3)
 
-                track_ids = result[0].boxes.id.int().cpu().tolist()
+                track_ids = r.boxes.id.int().cpu().tolist()
 
                 LINE_Y = int(height - 100)
                 # Tracking and Counting
@@ -80,15 +79,19 @@ def process_image_sequence(model_path, sequence_folder, output_path):
                     points = np.hstack(track).astype(np.int32).reshape((-1, 1, 2))
 
                     cv2.polylines(annotated_frame, [points], False, (0, 255, 255), 2)
-            cv2.putText(annotated_frame, str(crossed_count), (40, 60), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (255, 255, 0), 3)
+            cv2.putText(annotated_frame, str(crossed_count), (40, 60), cv2.FONT_HERSHEY_SIMPLEX, 2.5, (255, 255, 0), 4)
             out.write(annotated_frame)
 
-        out.release()
+        cv2.waitKey(1)
+
+    cap.release()
+    out.release()
+    cv2.destroyAllWindows()
 
 
 if __name__ == '__main__':
     MODEL_WEIGHTS = "weights/best.pt"
-    INPUT_FOLDER = "data/sample_sequence"
-    OUTPUT_VIDEO = "output_sequence.mp4"
+    INPUT_VIDEO = "data/sample_video.mp4"
+    OUTPUT_VIDEO = "output_video.mp4"
 
-    process_image_sequence(MODEL_WEIGHTS, INPUT_FOLDER, OUTPUT_VIDEO)
+    process_video(MODEL_WEIGHTS, INPUT_VIDEO, OUTPUT_VIDEO)
